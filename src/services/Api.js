e082e8db4cdaad1d8c8b7b473000f410e3aaf482
tafelli <guilhermetafelli@gmail.com>
2020-12-05 15:56:34 -0300

    teste

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getToken } from "./Auth";
 
-const api = axios.create({ baseURL: process.env.API_URL || 'http://localhost:8081'})
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL})
 
 api.interceptors.request.use(async config => {
   const token = getToken();
