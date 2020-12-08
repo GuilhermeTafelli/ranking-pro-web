db9275bcddbd64c5f9d0bb443a92ecc439f226d9
tafelli <guilhermetafelli@gmail.com>
2020-12-08 08:46:02 -0300

    testes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -319,8 +319,8 @@ export default function Home() {
 
     useEffect(async () => {
         setLoading(true)
-        const response = await api.get("/socials-media/ranking")
-        console.log(response.data.socialsMedia.slice(0, 5))
+        // const response = await api.get("/socials-media/ranking")
+        // console.log(response.data.socialsMedia.slice(0, 5))
         await new Promise((resolve) => setTimeout(resolve, 500))
         await setContent(["as", ["a"]])
         console.log("1")
